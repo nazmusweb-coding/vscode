4cad2bc7bbb5eb543a05e0c7a540454b91676e8f
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2024-05-29 19:25:52 +0000

    Two ways of debugging

---
+++ CS50/Week 2/Debugging/using_debugger.cpp
@@ -0,0 +1,17 @@
+// This is actually buggy0
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    // I need to print 3 grids
+    for (int i = 0; i <= 3; i++)
+    {
+        cout << "#" << endl;        // add breakpoint
+    }
+}
+
+// use command make program_name
+// Run and debug 
+// using the 4 buttons see analyze the code
---
+++ CS50/Week 2/Debugging/using_printf.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    // I need to print 3 grids
+    for (int i = 0; i <= 3; i++)
+    {
+        cout << "i is " << i << endl;   // for debugging
+        cout << "#" << endl;
+    }
+}

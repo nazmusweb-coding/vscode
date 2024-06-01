3b97c34d17c29af3ed1ea27452b575b03ca0747e
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2024-06-01 17:08:40 +0000

    Checked some example to clear null and last [] index confusion

--- CS50/Week 2/Arrays/char_array.cpp
+++ CS50/Week 2/Arrays/char_array.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(void)
 {   
     // string is a array of characters
-    string str = "Hi!";
+    string str = "HI!";
 
     // an extra byte is used to track the end of the string
     // which is NUL('\0')
@@ -22,5 +22,13 @@ int main(void)
     printf("%d %d %d\n", c1, c2, c3);
     // Output will be: 72 73 33
 
-    
+    printf("%d %d %d %d\n", str[0], str[1], str[2], str[3]);
+    // Output will be: 72 73 33 0
+    // so the matter of extra byte is clear now its for NUL (ascii value 0)
+    // for example:
+    int array[3] = { 72, 73, 33 };
+    printf("%d %d %d %d\n", array[0], array[1], array[2], array[3]);
+    // Output will be: 72 73 33 garbage value, last value isn't allocated 
+    // but litterally indicates the ending of the array like null in char array according to David
+
 }

5f11375843107a9ebdff990764e798cb9f23a3ea
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2024-06-02 15:38:16 +0000

    echo 0 command and returning 1 if we see required arguments are not typed

---
+++ CS50/Week 2/Command line arguments/status.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("Missing command-line argument\n");
+        return 1;
+    }
+    else
+    {
+        printf("Hello, %s\n", argv[1]);
+    }
+}
